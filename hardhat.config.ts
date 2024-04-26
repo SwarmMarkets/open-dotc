56b5b002f1fc4884411c4964e420fd12e2083f05
Bohdan <pukhnobg@gmail.com>
2024-04-26 14:26:22 +0300

    upd: Env for the project

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,32 +3,31 @@ import '@openzeppelin/hardhat-upgrades';
 import '@nomicfoundation/hardhat-toolbox';
 import 'solidity-docgen';
 import 'hardhat-contract-sizer';
-import './tasks/accounts';
-import './tasks/clean';
 import dotenv from 'dotenv';
 import { HardhatUserConfig } from 'hardhat/config';
+import { getNetworkConfig, AccountTypes } from './utils/get-network-account';
 dotenv.config();
-
-// Ensure that we have all the environment variables we need.
-const mnemonic = process.env.MNEMONIC ?? '';
-const swarmx_privateKey = process.env.SWARMX_DEPLOYER_PK ?? '';
-const testnet_privateKey = process.env.TESTNET_DEPLOYER_PK ?? '';
 
 const etherscanKey = process.env.ETHSCAN_KEY ?? '';
 const polyscanKey = process.env.POLYSCAN_KEY ?? '';
 const basescanKey = process.env.BASESCAN_KEY ?? '';
+const opscanKey = process.env.OPSCAN_KEY ?? '';
+const arbitrumscanKey = process.env.ARBSCAN_KEY ?? '';
 
-const infuraKey = process.env.INFURA_KEY ?? '';
-export const ethApiKey = process.env.ALCHEMY_KEY_ETH ?? infuraKey;
-const sepoliaApiKey = process.env.ALCHEMY_KEY_SEPOLIA ?? infuraKey;
-const polygonApiKey = process.env.ALCHEMY_KEY_POLYGON ?? infuraKey;
-const mumbaiApiKey = process.env.ALCHEMY_KEY_MUMBAI ?? infuraKey;
-const baseApiKey = process.env.ALCHEMY_KEY_BASE ?? infuraKey;
-const base_sepoliaApiKey = process.env.ALCHEMY_KEY_BASE_SEPOLIA ?? infuraKey;
+export const ethApiKey = process.env.ALCHEMY_KEY_ETH;
 
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [
+      {
+        version: '0.8.24',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
       {
         version: '0.8.19',
         settings: {
@@ -52,60 +51,27 @@ const config: HardhatUserConfig = {
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {
-      blockGasLimit: 20000000,
       throwOnCallFailures: false,
       chainId: 31337,
       initialBaseFeePerGas: 0,
       accounts: {
-        mnemonic,
         accountsBalance: '10000000000000000000000000',
       },
       forking: {
-        url: `https://eth-mainnet.alchemyapi.io/v2/${ethApiKey}`,
+        url: getNetworkConfig('mainnet', AccountTypes.SwarmMnemonic).url!,
         enabled: false,
         //blockNumber: 16383055,
       },
     },
-    mainnet: {
-      url: `https://eth-mainnet.alchemyapi.io/v2/${ethApiKey}`,
-      chainId: 1,
-      accounts: [swarmx_privateKey],
-      gas: 2100000,
-      gasPrice: 45000000000, // 45
-    },
-    sepolia: {
-      url: `https://eth-sepolia.g.alchemy.com/v2/${sepoliaApiKey}`,
-      chainId: 11155111,
-      accounts: [testnet_privateKey],
-      gas: 2100000,
-      gasPrice: 45000000000, // 45
-    },
-    polygon: {
-      url: `https://polygon-mainnet.g.alchemy.com/v2/${polygonApiKey}`,
-      chainId: 137,
-      accounts: { mnemonic },
-      gas: 5000000,
-      gasPrice: 250000000000, // 250
-    },
-    mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${mumbaiApiKey}`,
-      chainId: 80001,
-      accounts: [testnet_privateKey],
-      gas: 2100000,
-      gasPrice: 45000000000, // 45
-      gasMultiplier: 2,
-    },
-    base: {
-      url: `https://base-mainnet.g.alchemy.com/v2/${baseApiKey}`,
-      chainId: 8453,
-      accounts: [swarmx_privateKey],
-    },
-    base_sepolia: {
-      url: `https://base-sepolia.g.alchemy.com/v2/${base_sepoliaApiKey}`,
-      chainId: 84532,
-      accounts: [testnet_privateKey],
-      gasPrice: 30000000000, // 45
-    },
+    mainnet: getNetworkConfig('mainnet', AccountTypes.SwarmXMnemonic),
+    polygon: getNetworkConfig('polygon', AccountTypes.SwarmXMnemonic),
+    optimism: getNetworkConfig('optimism', AccountTypes.SwarmXMnemonic),
+    base: getNetworkConfig('base', AccountTypes.SwarmXMnemonic),
+    arbitrum: getNetworkConfig('arbitrum', AccountTypes.SwarmXMnemonic),
+    arbitrum_sepolia: getNetworkConfig('arbitrum_sepolia', AccountTypes.TestnetPk),
+    sepolia: getNetworkConfig('sepolia', AccountTypes.TestnetPk),
+    mumbai: getNetworkConfig('mumbai', AccountTypes.TestnetPk),
+    base_sepolia: getNetworkConfig('base_sepolia', AccountTypes.TestnetPk),
   },
   gasReporter: {
     coinmarketcap: process.env.COIN_MARKET_CAP_KEY,
@@ -122,8 +88,12 @@ const config: HardhatUserConfig = {
     apiKey: {
       mainnet: etherscanKey,
       polygon: polyscanKey,
+      optimisticEthereum: opscanKey,
       base: basescanKey,
+      arbitrumOne: arbitrumscanKey,
       base_sepolia: basescanKey,
+      sepolia: etherscanKey,
+      arbitrum_sepolia: arbitrumscanKey,
     },
     customChains: [
       {
@@ -140,6 +110,14 @@ const config: HardhatUserConfig = {
         urls: {
           apiURL: 'https://api-sepolia.basescan.org/api',
           browserURL: 'https://sepolia.basescan.org',
+        },
+      },
+      {
+        network: 'arbitrum_sepolia',
+        chainId: 421614,
+        urls: {
+          apiURL: 'https://api-sepolia.arbiscan.io/api',
+          browserURL: 'https://sepolia.arbiscan.io',
         },
       },
     ],
