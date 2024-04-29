054272703bcfca06f3d6c30634b247de6c39b2bb
Bohdan <pukhnobg@gmail.com>
2024-04-29 14:15:18 +0300

    coverage: exclude 1st Dotc version

--- .solcover.js
+++ .solcover.js
@@ -19,5 +19,5 @@ module.exports = {
   providerOptions: {
     mnemonic,
   },
-  skipFiles: ['mocks', 'test', 'mockFeeds', 'permissioning/PermissionManagerV3.sol'],
+  skipFiles: ['mocks', 'test', 'OpenDotc/Dotc.sol', 'OpenDotc/structures/DotcStructures.sol'],
 };
