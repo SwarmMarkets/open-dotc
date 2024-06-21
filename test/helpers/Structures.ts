6f9777c9d7a3a71391ade19524f97682e5159b3f
Bohdan Pukhno <pukhnobg@gmail.com>
2024-06-21 18:48:45 +0300

    separate: structures for v1,v2 and v3

--- test/helpers/Structures.ts
+++ test/helpers/Structures.ts
@@ -1,5 +1,15 @@
 import { BigNumberish } from 'ethers';
 
+export interface DotcOfferV3Struct {
+  maker: string;
+  isFullyTaken: boolean;
+  availableAmount: BigNumberish;
+  unitPrice: BigNumberish;
+  depositAsset: AssetStruct;
+  withdrawalAsset: AssetStruct;
+  offer: OfferStruct;
+}
+
 export interface DotcOfferV2Struct {
   maker: string;
   isFullyTaken: boolean;
@@ -32,12 +42,21 @@ export interface OfferStruct {
   commsLink: string;
 }
 
+export interface AssetV3Struct {
+  assetType: AssetType;
+  assetAddress: string;
+  amount: BigNumberish;
+  tokenId: BigNumberish;
+}
+
 export interface AssetStruct {
   assetType: AssetType;
   assetAddress: string;
   amount: BigNumberish;
   tokenId: BigNumberish;
 }
+
+
 
 export enum AssetType {
   NoType,
@@ -46,9 +65,35 @@ export enum AssetType {
   ERC1155,
 }
 
+export enum OfferPricingType {
+  NoType,
+  FixedPricing,
+  DynamicPricing
+}
+
+export enum EscrowV3CallType {
+  NoType,
+  Deposit,
+  Withdraw,
+  WithdrawFees,
+  Cancel
+}
+
 export enum EscrowCallType {
   Deposit,
   Withdraw,
   WithdrawFees,
   Cancel,
+}
+
+export enum ValidityType {
+  NotExist,
+  FullyTaken
+}
+
+export enum TimeConstraintType {
+  Expired,
+  TimelockGreaterThanExpirationTime,
+  InTimelock,
+  IncorrectTimelock
 }
---
+++ test/helpers/StructuresV3.ts
@@ -0,0 +1,74 @@
+import { BigNumberish } from 'ethers';
+
+export enum AssetType {
+	NoType,
+	ERC20,
+	ERC721,
+	ERC1155,
+}
+
+export enum OfferPricingType {
+	NoType,
+	FixedPricing,
+	DynamicPricing
+}
+
+export enum TakingOfferType {
+	NoType,
+	PartialTaking,
+	FullyTaking
+}
+
+export enum EscrowCallType {
+	NoType,
+	Deposit,
+	Withdraw,
+	WithdrawFees,
+	Cancel
+}
+export enum ValidityType {
+	NotExist,
+	PartiallyTaken,
+	FullyTaken
+}
+
+export enum TimeConstraintType {
+	Expired,
+	TimelockGreaterThanExpirationTime,
+	InTimelock,
+	IncorrectTimelock
+}
+
+export interface AssetStruct {
+	assetType: AssetType;
+	assetAddress: string;
+	assetPriceFeedAddress: string;
+	amount: BigNumberish;
+	tokenId: BigNumberish;
+}
+
+export interface OfferStruct {
+	takingOfferType: TakingOfferType;
+	offerPricingType: OfferPricingType;
+	specialAddresses: string[];
+	authorizationAddresses: string[];
+	expiryTimestamp: BigNumberish;
+	timelockPeriod: BigNumberish;
+	terms: string;
+	commsLink: string;
+}
+
+export interface DotcOfferStruct {
+	maker: string;
+	validityType: ValidityType;
+	availableAmount: BigNumberish;
+	unitPrice: BigNumberish;
+	depositAsset: AssetStruct;
+	withdrawalAsset: AssetStruct;
+	offer: OfferStruct;
+}
+
+
+
+
+
