be9dcb393f84afcb7aa2d5f190f7a032ee16fefc
Bohdan Pukhno <pukhnobg@gmail.com>
2024-07-08 16:06:37 +0300

    fix: price feed mocks

--- contracts/mocks/PriceFeedMock.sol
+++ contracts/mocks/PriceFeedMock.sol
@@ -4,7 +4,16 @@ pragma solidity ^0.8.25;
 import { IDotcCompatiblePriceFeed } from "../OpenDotc/v3/interfaces/IDotcCompatiblePriceFeed.sol";
 
 contract PriceFeedMock {
+    int256 value;
+    constructor(int256 _value) {
+        value = _value;
+    }
+
     function latestAnswer() external view returns (int256) {
-        return 777;
+        return value;
+    }
+
+    function decimals() external view returns (uint8) {
+        return 8;
     }
 }
