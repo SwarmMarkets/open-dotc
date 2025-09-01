9e6ed5220ba065ad5a2a44156ffc48de77aa6091
Bohdan Pukhno <pukhnobg@gmail.com>
2025-09-01 15:22:48 +0300

    chore: config update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,7 +13,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       { version: '0.8.28', settings: { optimizer: { enabled: true, runs: 200 } } },
-      { version: '0.8.24', settings: { optimizer: { enabled: true, runs: 200 } } },
+      { version: '0.8.25', settings: { optimizer: { enabled: true, runs: 200 } } },
       { version: '0.8.19', settings: { optimizer: { enabled: true, runs: 200 } } },
     ],
   },
