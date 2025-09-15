ba9b316f1ec992853c6125096ee9feaae57a25cb
Bohdan Pukhno <pukhnobg@gmail.com>
2025-09-15 16:57:32 +0300

    perf: separated swap functionality

--- contracts/buyer-burner/ChildSwarmBuyerBurner.sol
+++ contracts/buyer-burner/ChildSwarmBuyerBurner.sol
@@ -3,17 +3,8 @@ pragma solidity ^0.8.28;
 
 import { Initializable } from "solady/src/utils/Initializable.sol";
 import { Ownable } from "solady/src/auth/Ownable.sol";
-import { SafeTransferLib } from "solady/src/utils/SafeTransferLib.sol";
-import { MetadataReaderLib } from "solady/src/utils/MetadataReaderLib.sol";
 
-import { AggregatorV2V3Interface } from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV2V3Interface.sol";
-
-import { IV3SwapFactory } from "./interfaces/IV3SwapFactory.sol";
-import { IV3SwapRouter } from "./interfaces/IV3SwapRouter.sol";
-import { IV3SwapQuoter } from "./interfaces/IV3SwapQuoter.sol";
-import { ITokenTransferor } from "./interfaces/ITokenTransferor.sol";
-
-import { BuyerBurnerStorage } from "./extensions/BuyerBurnerStorage.sol";
+import { BuyerBurnerSwapper } from "./extensions/BuyerBurnerSwapper.sol";
 import { BuyerBurnerDotcOfferMaker } from "./extensions/BuyerBurnerOfferMaker.sol";
 import { BuyerBurnerCCIPCaller } from "./extensions/BuyerBurnerCCIPCaller.sol";
 
@@ -23,21 +14,10 @@ import { BuyerBurnerCCIPCaller } from "./extensions/BuyerBurnerCCIPCaller.sol";
 contract ChildSwarmBuyerBurner is
     Initializable,
     Ownable,
-    BuyerBurnerStorage,
+    BuyerBurnerSwapper,
     BuyerBurnerDotcOfferMaker,
     BuyerBurnerCCIPCaller
 {
-    using SafeTransferLib for address;
-    using MetadataReaderLib for address;
-
-    error PriceFeedError(address priceFeed);
-    error TokenDecimalsError(address token, uint8 decimals);
-
-    /// @notice Emitted when a `token` is swapped to SMT using WETH9 as an intermediary.
-    /// @param amountOut The amount of SMT received.
-    event Swapped(address token, uint256 amountOut);
-    event ZeroBalance(address token);
-
     /// @param uniswapV3Factory The address of the Uniswap V3 factory.
     /// @param uniswapV3Router The address of the Uniswap V3 swap router.
     /// @param weth The address of the WETH9 token.
@@ -46,7 +26,7 @@ contract ChildSwarmBuyerBurner is
         DotcV2 dotc,
         CCIPConfig calldata ccipConfig,
         DEXType[] calldata dexTypes,
-        DexConfig[] calldata configs,
+        DexConfig[] calldata dexConfigs,
         address[] calldata depositTokens
     ) external initializer {
         _setCCIPConfig(ccipConfig);
@@ -61,70 +41,7 @@ contract ChildSwarmBuyerBurner is
     /// @dev Requires approval for spending `token`.
     /// @return fullAmountOut The amount of SMT burned.
     function swap(DEXType dexType) external returns (uint256 fullAmountOut) {
-        address[] memory _tokens = tokens;
-        DexConfig memory config = dexConfigs[dexType];
-
-        for (uint256 i = 0; i < _tokens.length; ++i) {
-            uint256 amountIn = _tokens[i].balanceOf(address(this));
-            if (amountIn == 0) {
-                emit ZeroBalance(_tokens[i]);
-                continue; // Skip if no tokens are available for swapping
-            }
-
-            bool isWrappedNative = _tokens[i] == config.intermediateToken;
-
-            bytes memory path;
-
-            if (isWrappedNative) {
-                path = abi.encodePacked(config.intermediateToken, config.poolFee, config.finalToken);
-            } else {
-                if (
-                    ISwapV3Factory(config.swapV3Factory).getPool(
-                        _tokens[i],
-                        config.intermediateToken,
-                        config.poolFee
-                    ) == address(0)
-                ) {
-                    uint256 amountOut = 1; //TODO: take this 1 from getPrice() that relies on AggregatorV2V3Interface
-                    _makeOffer(_tokens[i], amountIn, config.finalToken, amountOut);
-
-                    continue;
-                }
-                path = abi.encodePacked(
-                    _tokens[i],
-                    config.poolFee,
-                    config.intermediateToken,
-                    config.poolFee,
-                    config.finalToken
-                );
-            }
-
-            uint256 amountOutMinimum = IV3SwapQuoter(config.swapV3Quoter).quoteExactInput(path, amountIn);
-
-            // Multiple pool swaps are encoded through bytes called a `path`.
-            // A path is a sequence of token addresses and POOL_FEEs that define the pools used in the swaps.
-            //
-            // The format for pool encoding is (tokenIn, fee, tokenOut/tokenIn, fee, tokenOut)
-            // where tokenIn/tokenOut parameter is the shared token across the pools.
-            //
-            // Since we are swapping `tokens[i]` to WETH9 and then WETH9 to SMT the path encoding
-            // is (`tokens[i]`, 0.3%, WETH9, 0.3%, SMT).
-            ISwapV3Router.ExactInputParams memory params = ISwapV3Router.ExactInputParams({
-                path: path,
-                recipient: address(this),
-                deadline: block.timestamp,
-                amountIn: amountIn,
-                amountOutMinimum: amountOutMinimum
-            });
-
-            uint256 amountOut = ISwapV3Router(config.swapV3Router).exactInput(params);
-            fullAmountOut += amountOut;
-
-            emit Swapped(tokens[i], amountOut);
-        }
-
-        // Withdraw the SMT tokens received from the swap.
-        _ccipTransfer(config.finalToken, fullAmountOut);
+        _swap(dexType);
     }
 
     /// @notice Adds a list of tokens to the whitelist.
@@ -151,6 +68,14 @@ contract ChildSwarmBuyerBurner is
     /// @param amount The amount of the `token`s to withdraw.
     function withdrawTokens(address token, address to, uint256 amount) external onlyOwner {
         token.safeTransfer(to, amount);
+    }
+
+    function _ifPoolsNA(address tokenIn, uint256 amountIn, address tokenOut, uint256 amountOut) internal override {
+        _makeOffer(_tokens[i], amountIn, config.finalToken, amountOut);
+    }
+
+    function _finishSwap(address token, uint256 amount) internal override {
+        _ccipTransfer(token, amount);
     }
 
     function _toApprove() internal view override returns (address) {
