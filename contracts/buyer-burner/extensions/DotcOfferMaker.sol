6485dfcc54afd65d88a94f67bb2bfeeb1cbf0ee0
Bohdan Pukhno <pukhnobg@gmail.com>
2025-09-15 15:00:36 +0300

    feat: add dotc maker

---
+++ contracts/buyer-burner/extensions/DotcOfferMaker.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.28;
+
+import { Asset, AssetType, AssetPrice, OfferStruct, TakingOfferType, OfferPrice } from "../../OpenDotc/v2/structures/DotcStructuresV2.sol";
+
+abstract contract DotcOfferMaker {
+    event OfferMade(
+        uint256 offerId,
+        address depositToken,
+        uint256 amountIn,
+        address withdrawalToken,
+        uint256 amountOut
+    );
+
+    DotcV2 public dotc;
+
+    function __initialize_DotcOfferMaker_(address _dotc) internal {
+        dotc = DotcV2(_dotc);
+    }
+
+    function _makeOffer(
+        address depositToken,
+        uint256 amountIn,
+        address withdrawalToken,
+        uint256 amountOut
+    ) internal virtual returns (uint256 offerId) {
+        Asset memory depositAsset = Asset({
+            assetType: AssetType.ERC20,
+            assetAddress: depositToken,
+            amount: amountIn,
+            tokenId: 0,
+            assetPrice: AssetPrice(address(0), 0, 0)
+        });
+
+        Asset memory withdrawalAsset = Asset({
+            assetType: AssetType.ERC20,
+            assetAddress: withdrawalToken,
+            amount: amountOut, //TODO: take from getPrice() that relies on AggregatorV2V3Interface,
+            tokenId: 0,
+            assetPrice: AssetPrice(address(0), 0, 0)
+        });
+
+        address[] memory addresses = address[](0);
+
+        OfferStruct memory offer = OfferStruct({
+            takingOfferType: TakingOfferType.PartialOffer,
+            offerPrice: OfferPrice(OfferPricingType.FixedPricing, 1, 0, PercentageType.NoType), // TODO: change uintPrice if required
+            specialAddresses: addresses,
+            authorizationAddresses: addresses,
+            expiryTimestamp: 0,
+            timelockPeriod: 0,
+            terms: TERMS,
+            commsLink: COMMS
+        });
+
+        offerId = dotc.currentOfferId() + 1;
+
+        dotc.makeOffer(depositAsset, withdrawalAsset, offer);
+
+        emit OfferMade(offerId, depositToken, amountIn, withdrawalToken, amountOut);
+    }
+}
