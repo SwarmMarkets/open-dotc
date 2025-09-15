7d911296a6788004a98aa0f38dcbd326f532a141
Bohdan Pukhno <pukhnobg@gmail.com>
2025-09-15 16:24:33 +0300

    feat: ccip caller

---
+++ contracts/buyer-burner/extensions/BuyerBurnerCCIPCaller.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.28;
+
+import { SafeTransferLib } from "solady/src/utils/SafeTransferLib.sol";
+
+import { ITokenTransferor } from "../interfaces/ITokenTransferor.sol";
+
+abstract contract BuyerBurnerCCIPCaller {
+    event CcipConfigSet(CCIPConfig config);
+    event CCIPTransferSubmited(bytes32 messageId, address token, uint256 amount);
+
+    struct CCIPConfig {
+        uint64 destinationChainSelector;
+        ITokenTransferor bridge;
+        address receiver;
+    }
+
+    CCIPConfig internal _ccipConfig;
+
+    function _setCCIPConfig(CCIPConfig calldata ccipConfig) internal {
+        _ccipConfig = ccipConfig;
+        emit CcipConfigSet(ccipConfig);
+    }
+
+    function _ccipTransfer(address token, uint256 amount) internal virtual {
+        uint256 fees = _estimateFees(token, amount);
+        bytes32 messageId = _ccipConfig.bridge.bridgeTokens{ value: fees }(
+            _ccipConfig.destinationChainSelector,
+            _ccipConfig.receiver,
+            token,
+            amount
+        );
+
+        if (fees < msg.value) {
+            SafeTransferLib.safeTransferETH(msg.sender, msg.value - fees);
+        }
+        emit CCIPTransferSubmited(messageId, token, amount);
+    }
+
+    function _estimateFees(address token, uint256 amount) internal view returns (uint256 fees) {
+        fees = _ccipConfig.bridge.estimateFees(
+            _ccipConfig.destinationChainSelector,
+            _ccipConfig.receiver,
+            token,
+            amount
+        );
+    }
+}
