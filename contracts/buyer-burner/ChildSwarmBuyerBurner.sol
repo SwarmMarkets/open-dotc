0493da35e19c21c4a84a3383be90c2d3fc11b184
Bohdan Pukhno <pukhnobg@gmail.com>
2025-09-15 15:06:52 +0300

    chore: update pragma

--- contracts/buyer-burner/ChildSwarmBuyerBurner.sol
+++ contracts/buyer-burner/ChildSwarmBuyerBurner.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.28;
 
 import { Initializable } from "solady/src/utils/Initializable.sol";
 import { Ownable } from "solady/src/auth/Ownable.sol";
