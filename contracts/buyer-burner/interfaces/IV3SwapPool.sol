d7169bd5b33a21d22141e7601424fd2c8aa06d7c
Bohdan Pukhno <pukhnobg@gmail.com>
2025-12-03 17:46:55 +0200

    feat: add slot0 function to interface

--- contracts/buyer-burner/interfaces/IV3SwapPool.sol
+++ contracts/buyer-burner/interfaces/IV3SwapPool.sol
@@ -3,4 +3,17 @@ pragma solidity ^0.8.25;
 
 interface IV3SwapPool {
     function liquidity() external view returns (uint128);
+
+    function slot0()
+        external
+        view
+        returns (
+            uint160 sqrtPriceX96,
+            int24 tick,
+            uint16 observationIndex,
+            uint16 observationCardinality,
+            uint16 observationCardinalityNext,
+            uint8 feeProtocol,
+            bool unlocked
+        );
 }
